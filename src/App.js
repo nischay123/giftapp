bdd5cb3cce9fbb35a7d1f643ecf1e4b0d82c2755
shashank <nischaygarg200@gmail.com>
2021-07-25 17:49:46 +0530

    add Gift added

--- src/App.js
+++ src/App.js
@@ -7,12 +7,15 @@ import 'bootstrap/dist/css/bootstrap.css';
 function App() {
 
   const [gift, setgift] = useState([
-    { id: "1", name: "gift 1", description:  "1 Gift-giving is something that should not be a burden. ..", price: '12' },
+    { id: "1", name: "gift 1", description: "1 Gift-giving is something that should not be a burden. ..", price: '12' },
     { id: "2", name: "gift 2", description: "2 Gift-giving is something that should not be a burden. ...", price: '302' },
     { id: "3", name: "gift 3", description: "3 Gift-giving is something that should not be a burden. ", price: '40' },
     { id: "4", name: "gift 4", description: "4 Gift-giving is something that should not be a burden. ... When ", price: '90' }
   ]);
   const [EditContent, setEditContent] = useState(null);
+  const [addContent, setaddContent] = useState(null);
+
+  const [toggle, setToggle] = useState(false)
 
   const handleDelete = (id) => {
     const index = gift.findIndex(ele => {
@@ -22,7 +25,28 @@ function App() {
     const newGift = [...gift];
     newGift.splice(index, 1);
     console.log(newGift);
-    setgift(newGift)
+    setgift(newGift);
+    setEditContent(null);
+
+  }
+
+  const handleAdd = () => {
+    console.log(addContent);
+    const newE = {
+      ...gift,
+      id: Math.random()
+    }
+    const newa = [...gift];
+    newa.push(newE)
+    setgift(newa);
+    setToggle(!toggle);
+
+  }
+  const handleAChange = (e) => {
+    setaddContent({
+      ...addContent,
+      [e.target.name]: e.target.value
+    })
   }
 
   const handleEdit = (id) => {
@@ -37,20 +61,22 @@ function App() {
       [e.target.name]: e.target.value
     })
   }
-const handleUpdate=()=>{
-  if(EditContent !== null){
-    const index = gift.findIndex(ele => {
-      return ele.id === EditContent.id;
-    });
-    const updategift = gift[index];
+  const handleUpdate = () => {
+    if (EditContent !== null) {
+      const index = gift.findIndex(ele => {
+        return ele.id === EditContent.id;
+      });
+      const updategift = gift[index];
 
-    const nwGift = [...gift];
-    nwGift.splice(index,1,EditContent);
-    setgift(nwGift);
-    setEditContent(null);
-    console.log(EditContent);
+      const nwGift = [...gift];
+      nwGift.splice(index, 1, EditContent);
+      setgift(nwGift);
+      setEditContent(null);
+      console.log(EditContent);
+    }
   }
-}
+
+  console.log(gift);
   let content = gift.map(g => {
     return <Card
       id={g.id}
@@ -64,11 +90,11 @@ const handleUpdate=()=>{
   if (EditContent !== null) {
     EditForm = (
       <form>
-        <div class="form-group">
-          <label for="exampleInputEmail1">Name Of Gift</label>
+        <div className="form-group">
+          <label forName="exampleInputEmail1">Name Of Gift</label>
           <input
             type="text"
-            class="form-control"
+            className="form-control"
             id="exampleInputEmail1"
             placeholder="Enter Gift name"
             value={EditContent.name}
@@ -76,11 +102,11 @@ const handleUpdate=()=>{
             onChange={(e) => handleChange(e)}
           />
         </div>
-        <div class="form-group">
-          <label for="exampleInputEmail1">Prices</label>
+        <div className="form-group">
+          <label forName="exampleInputEmail1">Prices</label>
           <input
             type="text"
-            class="form-control"
+            className="form-control"
             id="exampleInputEmail1"
             placeholder="Enterprice"
             value={EditContent.price}
@@ -88,17 +114,17 @@ const handleUpdate=()=>{
             onChange={(e) => handleChange(e)}
           />
         </div>
-        <div class="form-group">
-          <label for="exampleInputEmail1">Description</label>
+        <div className="form-group">
+          <label forName="exampleInputEmail1">Description</label>
           <textarea
-            class="form-control"
+            className="form-control"
             id="exampleFormControlTextarea1"
             value={EditContent.description}
             name="description"
             onChange={(e) => handleChange(e)}
             rows="3"></textarea>
         </div>
-        <button type="button" class="btn btn-primary my-2" onClick={(e)=>handleUpdate(e)}>update</button>
+        <button type="button" className="btn btn-primary my-2" onClick={(e) => handleUpdate(e)}>update</button>
       </form>
     )
   }
@@ -112,15 +138,56 @@ const handleUpdate=()=>{
         <p class="lead">This is a simple hero unit, a simple</p>
         <hr class="my-4" />
         {/* <p>It uses utility classes for typography and spacing to space content out within the larger container.</p> */}
-
+        {!toggle && <button type="button" class="btn mb-2 btn-primary " onClick={() => setToggle(!toggle)}>Add New</button>
+        }
       </div>
-      {EditForm}
+      {
+        toggle && <form>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Name Of Gift</label>
+            <input
+              type="text"
+              class="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter Gift name"
+              name="name"
+              onChange={(e) => handleAChange(e)}
+            />
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Prices</label>
+            <input
+              type="text"
+              class="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enterprice"
+              name="price"
+              onChange={(e) => handleAChange(e)}
+            />
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Description</label>
+            <textarea
+              class="form-control"
+              id="exampleFormControlTextarea1"
+              name="description"
+              onChange={(e) => handleAChange(e)}
+              rows="3"></textarea>
+          </div>
+
+          <button type="button" class="btn btn-primary my-2" onClick={(e) => handleAdd(e)}> Save </button>
+        </form>
+
+      }
       <div className="App" >
-      <div class="row">
+        <div class="row">
 
-        {content}
+          {content}
         </div>
+        {EditForm}
+
       </div>
+      
     </div>
 
   );
